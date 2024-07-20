f298b1938839db34f1f1058b97d6a10c0e6b1162
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-07-20 16:39:50 +0500

    updated

--- frontend/src/Pages/Listing.jsx
+++ frontend/src/Pages/Listing.jsx
@@ -8,13 +8,21 @@ const Listing = () => {
   const { data: properties, isError, isLoading } = useProperties();
   const [category, setCategory] = useState("All");
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
 
   const handleCategoryChange = (newCategory) => {
     setCategory(newCategory);
+    if (newCategory === "All") {
+      setSelectedCity(""); // Reset selected city when "All" category is selected
+    }
   };
 
   const handleSearchChange = (term) => {
     setSearchTerm(term);
+  };
+
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
   };
 
   if (isLoading) {
@@ -37,7 +45,8 @@ const Listing = () => {
       property.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
       property.city.toLowerCase().includes(searchTerm.toLowerCase()) ||
       property.country.toLowerCase().includes(searchTerm.toLowerCase());
-    return matchesCategory && matchesSearchTerm;
+    const matchesCity = selectedCity === "" || property.city === selectedCity;
+    return matchesCategory && matchesSearchTerm && matchesCity;
   });
 
   return (
@@ -74,6 +83,20 @@ const Listing = () => {
           >
             RENT
           </button>
+        </div>
+        <div className="flex mt-8">
+          <div className="flex-shrink-0">
+            <h4>City</h4>
+            <select
+              value={selectedCity}
+              onChange={handleCityChange}
+              className="border rounded-md p-2"
+            >
+              <option value="">All Cities</option>
+              <option value="Lahore">Lahore</option>
+              <option value="Multan">Multan</option>
+            </select>
+          </div>
         </div>
         {filteredProperties.length > 0 ? (
           <div className="grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-10">
