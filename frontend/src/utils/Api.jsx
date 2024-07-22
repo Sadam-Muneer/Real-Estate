9105c42d00c1e654544066e06cd56b407b9b4fef
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-07-22 11:07:33 +0500

    updated

--- frontend/src/utils/Api.jsx
+++ frontend/src/utils/Api.jsx
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://realestate-backend-psi-ebon.vercel.app/api",
+  baseURL: "https://realestate-yt-backend-psi-ebon.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
